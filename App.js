d78af3ad66654f4163f4800def4000184947b31e
JuanSBass <juan_santillan20@hotmail.com>
2023-01-17 22:42:39 -0600

    config styles

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import Mainpage from "./src/components/Mainpage.jsx"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Esto es una prueba!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Mainpage />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/StyledText.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import theme from "../theme"
+
+const styles = StyleSheet.create({
+  text: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.body,
+    fontFamily: theme.fonts.main,
+    fontWeight: theme.fontWeights.normal
+  },
+  bold: {
+    fontWeight: theme.fontWeights.bold
+  },
+  subheading: {
+    fontSize: theme.fontSizes.subHeading
+  },
+  colorPrimary: {
+    
+  }
+})
